70bfe85c0e6c37fedd424729f9ad06484bea337a
Nathan N <Nitharios@users.noreply.github.com>
2018-02-13 17:34:26 -1000

    updated README

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup as soup
 from urllib.request import FancyURLopener, urlopen
 from urllib.error import HTTPError
 
+# comment out or program will break
 from lib import script
 from assets.config import url, path
 
@@ -20,4 +21,5 @@ else:
   f.write(data)
   f.close
 
+# comment out or program will break
 print(script.run())
