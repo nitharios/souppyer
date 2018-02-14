c8dcd4bf43188e37b5d0d2e86c2f7a32f898d6af
Nathan N <Nitharios@users.noreply.github.com>
2018-02-13 17:03:18 -1000

    able to read and write data

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as soup
+from urllib.request import FancyURLopener, urlopen
+from urllib.error import HTTPError
+from assets.config import url, path
+
+try:
+  class AppURLopener(FancyURLopener):
+    version = "Mozilla/5.0"
+  opener = AppURLopener()
+  response = opener.open(url)
+
+except HTTPError as e:
+  print(e)
+
+else:
+  # print(response.read())
+  f = open(path, 'ab')
+  data = response.read()
+  f.write(data)
+  f.close
 
 from lib import script
 
